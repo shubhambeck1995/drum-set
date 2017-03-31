6f85435fd99ec730e8a3ca39933a8fad66330ab8
Shubham Beck <shubhambeck1995@gmail.com>
2017-04-01 00:15:18 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function changer(e) {
+    var key = e.keyCode;
+
+    function remover() {
+        document.getElementById("kk-" + key).classList.remove("playing");
+    }
+
+
+    console.log(key);
+    var elem = document.getElementById("kk-" + key);
+    elem.className += " playing";
+    var player = document.getElementById("ss-" + key);
+    player.play();
+
+
+    setTimeout(remover, 300);
+
+
+}
+
+window.addEventListener("keydown", changer);
+window.onLoad = changer;
